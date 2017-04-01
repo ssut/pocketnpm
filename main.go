14516493df95033d96a5168e711092eaac857662
SuHun Han <ssut@ssut.me>
2017-04-01 20:40:55 +0900

    main: add an option for disabling full mirroring
    this will be used to start only registry server without mirroring
    feature. added for the case where no internet connection is available.

--- main.go
+++ main.go
@@ -94,8 +94,9 @@ func main() {
 			Usage:   "Start PocketNPM with continuous mirroring mode",
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "config, c", Value: "config.toml"},
+				cli.BoolFlag{Name: "only-server", Usage: "Disable mirroring and start only registry server"},
 				cli.BoolFlag{Name: "onetime, o", Usage: "One-time mirroring (disable continuous mirroring)"},
-				cli.BoolFlag{Name: "server, s", Usage: "start NPM registry server"},
+				cli.BoolFlag{Name: "server, s", Usage: "Start NPM registry server"},
 			},
 			Action: func(c *cli.Context) error {
 				conf := getConfig(c.String("config"))
@@ -106,6 +107,10 @@ func main() {
 				// webserver
 				if c.Bool("server") {
 					server := npm.NewPocketServer(pb, &conf.Server, &conf.Mirror)
+					if c.Bool("only-server") {
+						server.Run()
+						return nil
+					}
 					go server.Run()
 				}
 
