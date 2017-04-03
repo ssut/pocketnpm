57eea832e09d3207712753bde93d9d361d98c0fc
SuHun Han <ssut@ssut.me>
2017-04-03 23:45:43 +0900

    utils: add hashSHA1 func to get sha1 hash of a file

--- npm/utils.go
+++ npm/utils.go
@@ -1,15 +1,25 @@
 package npm
 
 import (
+	"encoding/hex"
+	"io"
 	"path/filepath"
 	"strings"
+
+	"crypto/sha1"
 
 	"github.com/pquerna/ffjson/ffjson"
 )
 
 type distribution struct {
-	SHA1    string
-	Tarball string
+	SHA1      string
+	Tarball   string
+	Completed bool
+}
+
+type file interface {
+	io.Seeker
+	io.Reader
 }
 
 func getLocalPath(base string, path string) string {
@@ -36,10 +46,23 @@ func getDistributions(document string) []*distribution {
 	for _, version := range versions {
 		dist := version.(map[string]interface{})["dist"].(map[string]interface{})
 		distributions = append(distributions, &distribution{
-			SHA1:    dist["shasum"].(string),
-			Tarball: dist["tarball"].(string),
+			SHA1:      dist["shasum"].(string),
+			Tarball:   dist["tarball"].(string),
+			Completed: false,
 		})
 	}
 
 	return distributions
+}
+
+func hashSHA1(f file) (sha1str string, err error) {
+	hash := sha1.New()
+	if _, err = io.Copy(hash, f); err != nil {
+		return
+	}
+
+	hashInBytes := hash.Sum(nil)[:20]
+	sha1str = hex.EncodeToString(hashInBytes)
+	f.Seek(0, 0)
+	return
 }
--- npm/utils_test.go
+++ npm/utils_test.go
@@ -2,6 +2,7 @@ package npm
 
 import "testing"
 import "reflect"
+import "github.com/spf13/afero"
 
 func TestGetLocalPath(t *testing.T) {
 	tests := []struct {
@@ -43,12 +44,14 @@ func TestGetDistributions(t *testing.T) {
 			document: `{"_id": "test", "versions": {"0.0.1": {"dist": {"shasum": "3a16ee0d835eee3fbf97760efdfdbbe8fbfd4b3b", "tarball": "https://registry.npmjs.org/react/-/react.tgz"}}, "0.0.2": {"dist": {"shasum": "095de887016e2739a0773755f4ee6d8886c72ff3", "tarball": "https://registry.npmjs.org/react/-/react.tgz"}}}}`,
 			expected: []*distribution{
 				{
-					SHA1:    "3a16ee0d835eee3fbf97760efdfdbbe8fbfd4b3b",
-					Tarball: "https://registry.npmjs.org/react/-/react.tgz",
+					SHA1:      "3a16ee0d835eee3fbf97760efdfdbbe8fbfd4b3b",
+					Tarball:   "https://registry.npmjs.org/react/-/react.tgz",
+					Completed: false,
 				},
 				{
-					SHA1:    "095de887016e2739a0773755f4ee6d8886c72ff3",
-					Tarball: "https://registry.npmjs.org/react/-/react.tgz",
+					SHA1:      "095de887016e2739a0773755f4ee6d8886c72ff3",
+					Tarball:   "https://registry.npmjs.org/react/-/react.tgz",
+					Completed: false,
 				},
 			},
 		},
@@ -59,5 +62,17 @@ func TestGetDistributions(t *testing.T) {
 		if !reflect.DeepEqual(actual, test.expected) {
 			t.Errorf("getDistributions(%d): expected %q actual %q", i, test.expected, actual)
 		}
+	}
+}
+
+func TestHashSHA1(t *testing.T) {
+	expected := "242606355b6a6d18a5381b1c58aaf23e9e961851"
+	appFS := afero.NewMemMapFs()
+	afero.WriteFile(appFS, "test", []byte("PocketNPM"), 0644)
+	file, _ := appFS.Open("test")
+
+	actual, _ := hashSHA1(file)
+	if actual != expected {
+		t.Errorf("hashSHA1: expected %s actual %s", expected, actual)
 	}
 }
