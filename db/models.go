3f9f7e7bf8807d00a7f28e9d61046892043012e9
SuHun Han <ssut@ssut.me>
2017-04-04 22:55:19 +0900

    database: declare multiple backends

--- db/models.go
+++ db/models.go
@@ -1,5 +1,7 @@
 package db
 
+import "net/url"
+
 const (
 	MarkIncomplete = "0"
 	MarkComplete   = "1"
@@ -7,6 +9,7 @@ const (
 
 // DatabaseConfig defines config for database
 type DatabaseConfig struct {
+	Type          string `toml:"type"`
 	Path          string `toml:"path"`
 	MaxCacheSize  int    `toml:"max_cache_size"`
 	CacheLifetime int    `toml:"cache_lifetime"`
@@ -25,3 +28,33 @@ type BarePackage struct {
 	ID       string
 	Revision string
 }
+
+// PocketStore represents the database factory interface
+type PocketStore interface {
+	Connect()
+	Init()
+	IsInitialized() bool
+	GetItemCount(string) int
+	GetStats() *DatabaseStats
+	GetSequence() int
+	SetSequence(int)
+	GetCountOfMarks(bool) int
+	GetIncompletePackages() []*BarePackage
+	GetRevision(string) string
+	GetDocument(string, bool) (string, []*url.URL, error)
+	GetAllFiles() map[string][]*url.URL
+	PutPackage(string, string, bool, bool) error
+	PutPackages([]*BarePackage)
+	DeletePackage(string)
+	PutCompleted(*BarePackage, string, string, []*url.URL) bool
+}
+
+// StoreType represents the type for database store
+type StoreType int
+
+const (
+	// BoltStore uses boltdb as a database backend
+	BoltStore StoreType = 1 << iota
+	// GormStore uses gorm as a database backend controller
+	GormStore
+)
